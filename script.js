7f42a8968b2c23ccf60c1487e839bad256e836a1
wy-chan <79743156+wy-chan@users.noreply.github.com>
2022-02-17 11:43:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,97 @@
-class MyPage extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-      page:1,
-    };
-  }
-  render(){
-    return( 
-      <div>
- <div class="project-top tile">
-      <h3>Projects (3)</h3>
-      <button class="project-btn active-btn">Responsive</button>
-    </div>
-    <div class="project-box-group">
-    <div class="project-box tile">
-      <img class="project-img" src="https://raw.githubusercontent.com/wy-chan/devchallenges_Portfolio/main/images/screen_profilep1.png" alt="project image"/>
-      <p class="tag">#HTML #CSS #Responsive</p>
-      <h4>Portfolio Page</h4>
-      <p>In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io</p>
-      <div class="project-btn-s-box">
-      <button class="project-btn-s active-btn" onclick="window.open('https://wy-chan.github.io/devchallenges_Portfolio/','_blank')" >Demo</button>
-      <button class="project-btn-s" onclick="window.open('https://github.com/wy-chan/devchallenges_Portfolio','_blank')">Code</button>
-      </div>
-    </div>
-    
-        <div class="project-box tile">
-       <img class="project-img" src="https://raw.githubusercontent.com/wy-chan/devchallenges_Portfolio/main/images/screen_checkoutp.png" alt="project image"/>
-          <p class="tag">#HTML #CSS #Responsive</p>
-       <h4>Checkout Page</h4>
-           <p>In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io</p>
-          <div class="project-btn-s-box">
-      <button class="project-btn-s active-btn" onclick="window.open('https://wy-chan.github.io/devchallenges_CheckoutPage/','_blank')">Demo</button>
-      <button class="project-btn-s" onclick="window.open('https://github.com/wy-chan/devchallenges_CheckoutPage','_blank')">Code</button>
-      </div>
-    </div>
-    
-        <div class="project-box tile">
-       <img class="project-img" src="https://raw.githubusercontent.com/wy-chan/devchallenges_Portfolio/main/images/screen_ediehomep.png" alt="project image"/>
-          <p class="tag">#HTML #CSS #Responsive</p>
-          <h4>Edie Homepage</h4>
-           <p>In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io</p>
-          <div class="project-btn-s-box">
-      <button class="project-btn-s active-btn" onclick="window.open('https://wy-chan.github.io/devchallenges_EdieHomepage/','_blank')">Demo</button>
-      <button class="project-btn-s" onclick="window.open('https://github.com/wy-chan/devchallenges_EdieHomepage','_blank')">Code</button>
-      </div>
-    </div>
-     </div>
-    
-    <div class="page-btn-box">
-      <button class="page-btn "><span class="material-icons">
-chevron_left
-</span></button>
-      <button class="page-btn active-btn">1
-      </button>
-      <button class="page-btn">2
-      </button>
-       <button class="page-btn">3
-      </button>
-       <button class="page-btn "><span class="material-icons">
-chevron_right
-</span></button>
-    </div>
-</div>
+let projectsData;
 
-    );
-  }
+let dataURL = 'https://raw.githubusercontent.com/wy-chan/devchallenges_Portfolio/main1/projects.json';
+
+var currentPage= 0;
+
+
+function getPageData() {
+  return $.ajax({
+    headers: {
+      Accept: 'application/json'
+    },
+    url:
+      dataURL,
+    success: function (jsonData) {
+      if (typeof jsonData === 'string') {
+        projectsData = JSON.parse(jsonData);
+        console.log('projectsData');
+        console.log(projectsData);
+      }
+    }
+  });
 }
-class ProjectBox extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={};
-  }
-  render(){
-    
-  }
+function closePage(i){
+  var element = document.getElementsByClassName("toPage")[i];
+  element.classList.remove("active-btn");
 }
 
-ReactDOM.render(<MyPage />,document.getElementById('projects'));
+function nextPage(){
+  closePage(currentPage);
+  if (currentPage <2){
+    currentPage +=1;
+  };
+  getPage(currentPage);
+  console.log(currentPage);
+}
+function previousPage(){
+   closePage(currentPage);
+  if (currentPage >0){
+    currentPage -=1;
+  };
+  getPage(currentPage);
+  console.log(currentPage);
+}
+function toPage(i){
+   closePage(currentPage);
+  currentPage = i;
+   getPage(currentPage);
+  console.log(currentPage);
+}
 
+function getProjects(i){
+  return projectsData.projects[i];
+}
+
+function loadProject(i,p){
+  if(p == undefined){
+    $('.project-img').eq(i).attr('style','opacity:0');
+  $('.projectTitle').eq(i).attr('style','opacity:0');
+  $('.projectDemo').eq(i).attr('style','opacity:0');
+  $('.projectCode').eq(i).attr('style','opacity:0');
+  $('.tag').eq(i).attr('style','opacity:0');
+  $('.projectText').eq(i).attr('style','opacity:0');
+  }
+  else{
+  $('.tag').eq(i).attr('style','opacity:1');
+  $('.projectText').eq(i).attr('style','opacity:1');
+    $('.project-img').eq(i).attr('src',p.image).attr('style','opacity:1');
+  $('.projectTitle').eq(i).text(p.title).attr('style','opacity:1');
+  $('.projectDemo').eq(i).attr('onclick',"window.open('"+p.demoURL+"','_blank')" ).attr('style','opacity:1');
+  $('.projectCode').eq(i).attr('onclick',"window.open('"+p.codeURL+"','_blank')" ).attr('style','opacity:1');
+
+      }
+}
+
+function getPage(i) {
+  let projectA = getProjects(i*3);
+  let projectB = getProjects(i*3+1);
+  let projectC = getProjects(i*3+2);
+  loadProject(0,projectA);
+  loadProject(1,projectB);
+  loadProject(2,projectC);
+  var element = document.getElementsByClassName("toPage")[i];
+  element.classList.add("active-btn");
+}
+
+$(document).ready(function () {
+  getPageData().then(() => {
+    getPage(currentPage);
+  });
+
+  $('#btn-toLeft').on('click', previousPage);
+  $('#btn-toRight').on('click', nextPage);
+  $('.toPage').eq(0).on('click',function(){toPage(0)});
+  $('.toPage').eq(1).on('click',function(){toPage(1)});
+  $('.toPage').eq(2).on('click',function(){toPage(2)});
+});
